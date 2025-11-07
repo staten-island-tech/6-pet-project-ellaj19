16710146e0538288e0c58619a933e174e2dee575
ellaj19 <ellaj19@nycstudents.net>
2025-11-07 10:17:00 -0500

    a

--- test.py
+++ test.py
@@ -7,5 +7,18 @@ class Hero:
         self.inventory.append(item)
         print(self.inventory)
 mia = Hero("mia",278,["fat, light brown hamster named gerald"])
-mia.buy({"title":"poopy shoes", "atk":5})
+mia.buy({"title": "poopy shoes", "atk": 5})
 print(mia.__dict__)
+
+class pet:
+    def __init__(self, name, happiness, hunger, fear):
+        self.name = input("what is your pet cat's name?")
+        self.__happiness = happiness
+        self.__hunger = hunger
+        self.__fear = fear
+    def play(self):
+        self.__happiness == 0
+        print(f"do you want to play with {self.name}?")
+        if "yes":
+            self.__happiness += 10
+cat = pet("cat","cat",0,0)
