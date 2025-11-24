338526ad7bccf2a0cc795dbe90f3ebd0874c60c9
ellaj19 <ellaj19@nycstudents.net>
2025-11-24 10:16:59 -0500

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ mia.buy({"title": "poopy shoes", "atk": 5})
 print(mia.__dict__)
  """
 
-""" 
+""" """ 
 class pet:
     def __init__(self, name, happiness, clean, hunger, fear, age):
         self.name = name
@@ -21,8 +21,7 @@ class pet:
         self.fear = fear
         self.age = age
     def play(self):
-        self.__happiness == 0
-        question = input(f"do you want to play with {answer1}?")
+        question = input(f"do you want to play with {answer1}? yes/no")
         if question == "yes":
             self.__happiness += 10
             self.hunger +=5
@@ -33,7 +32,7 @@ class pet:
         else:
             print("invalid answer")
     def feed(self):
-        question1 = input(f"do you want to feed {answer1}?")
+        question1 = input(f"do you want to feed {answer1}? yes/no")
         if question1 == "yes":
             self.hunger -=10
             print("the hamster is getting bigger...")
@@ -42,39 +41,29 @@ class pet:
             print("wrong choice")
         else:
             print("invalid answer")
-
     def wash(self):
-        question2 = input(f"do you want to clean {answer1}?")
+        question2 = input(f"do you want to clean {answer1}? yes/no")
         if question2 == "yes":
             self.clean +=10
             print("thats not fur...")
         elif question2 == "no":
             self.clean -= 5 
             print("wash your hands")
-        else:
-            print("invalid answer")
-    def secret(self):
-        question3 = input("do you want to know a secret?")
-        if question3 == "yes":
-            self.fear +=10
-            print("wash the hamster")
-            hamster.wash()
-        elif question3 == "no":
-            self.clean -= 5 
-            print("wash your hands")
+        elif self.__happiness == 100:
+             print("great job taking care of your meal")
         else:
             print("invalid answer")
     def show_status(self):
-        self.age == 0
-        answer = input("it's the end of the day. do you want to see stats?")
+        answer = input("it's the end of the day. do you want to see stats? yes/no")
         if answer == "yes":
             self.age += 1
             print(f"{answer1}'s happiness is at {self.__happiness}%. {answer1}'s hunger is at {self.hunger}%. {answer1}'s fear is at {self.fear}%. {answer1}'s cleanliness is at {self.clean}%. {answer1}'s age is {self.age} days old")
         else:
+            self.age += 1
             print(f"okay. {answer1} is sleeping.")
 answer1 = input("what is your pet hamster's name?")
 print(f"hello, {answer1}")
-hamster = pet({answer1},50,50,25,25,0) 
+hamster = pet({answer1},50,50,25,25,12) 
 print(f"welcome home, {answer1}! let's interact with the hamster.")
 while hamster.age <= 24:
     hamster.play()
@@ -83,17 +72,21 @@ while hamster.age <= 24:
     hamster.show_status()
     if hamster.hunger <= -50:
         print("your hamster has died of obesity")
+        break
     elif hamster.hunger >= 100:
         print(f"uh oh, {answer1} has died of hunger")
+        break
     elif hamster.clean <= 0:
         print("your hamster passed away from its own odor")
+        break
     elif hamster.fear >= 100:
         print("your hamster died of terror")
+        break
     elif hamster.age == 24:
-        print("your hamster passed away of old age") """
+        print("your hamster passed away of old age")
+        break
 
-
-def isValid():
+""" def isValid():
     email = input("what is your email?")
     password = input("what is your password?")
     if "@" not in email:
@@ -106,9 +99,19 @@ def isValid():
        return "invalid password: needs to be at least 8 characters long"
     return{'email':email, 'password':password}
 
-print(isValid())
+print(isValid()) """
 
-
+def secret(self):
+        question3 = input("do you want to know a secret?")
+        if question3 == "yes":
+            self.fear +=10
+            print("wash the hamster")
+            hamster.wash()
+        elif question3 == "no":
+            self.clean -= 5 
+            print("wash your hands")
+        else:
+            print("invalid answer")
 
 
 
