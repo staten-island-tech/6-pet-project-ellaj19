1edfe916b768af22b2eb3e1e2e4e9d4a8ae5a29e
ellaj19 <ellaj19@nycstudents.net>
2026-03-12 22:29:20 -0400

    Update testtte.py

--- testtte.py
+++
@@ -1,8 +0,0 @@
-question = input(f"do you want to play with ? yes/no")
-while "yes" not in question and "no" not in question:
-            question = input(f"do you want to play with ? yes/no")
-if question == "yes":
-                print(". . .")
-elif question == "no":
-                
-                print(" .")
